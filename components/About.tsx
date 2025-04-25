1758eaffe0b18dd13216d298d3cf03def5ae1875
ntl-muzzamil <muzzamil.hussain@nayatel.com>
2025-04-25 15:07:14 +0500

    updates

---
+++ components/About.tsx
@@ -0,0 +1,23 @@
+"use client";
+/* eslint-disable */
+
+export default function About() {
+  return (
+    <section id="about" className="scroll-mt-16 ">
+      <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-background/0 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+        <h2 className="text-sm font-bold uppercase tracking-widest lg:sr-only">
+          About
+        </h2>
+      </div>
+      <div className="flex flex-col gap-4">
+        <p className="text-start text-muted-foreground lg:px-6">
+          I'm a full-stack problem solver with over 2 years who speaks fluent JavaScript, breathes APIs,
+          and dreams in clean architecture. From scalable backend systems to pixel-perfect UIs,
+          I build with performance, security, and user experience in mind.
+          I love turning complex challenges into elegant code, and I’m always chasing the next big
+          thing in tech. Let's build the future, one commit at a time.
+        </p>
+      </div>
+    </section>
+  );
+}
---
+++ components/ExpCards.tsx
@@ -0,0 +1,87 @@
+"use client";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { MoveRight } from "lucide-react";
+
+const jobPositions = [
+  {
+    timeline: "Oct 2023 — Present",
+    currentPosition: "Senior Software Engineer",
+    place: "Nayatel",
+    previousPositions: ["Junior Software Engineer", "Software Engineer"],
+    description:
+      "Senior Software Engineer at Nayatel, building scalable full-stack solutions with a focus on performance, clean architecture, and great user experiences. Skilled in React, Node.js, and UI/UX design, with a strong track record of team leadership and continuous growth from junior to senior roles.",
+    skills: [
+      "UI/UX Design",
+      "React",
+      "Javascript",
+      "Node",
+      "Software Architecture",
+      "Team Leadership",
+    ],
+  },
+];
+
+export default function ExpCard() {
+  return (
+    <section id="experience" className="scroll-mt-16 lg:mt-16">
+      <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-background/0 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+        <h2 className="text-sm font-bold uppercase tracking-widest lg:sr-only">
+          Experience
+        </h2>
+      </div>
+      <>
+        {jobPositions.map((job, index) => (
+          <Card
+            key={index}
+            className="lg:p-6 mb-4 flex flex-col lg:flex-row w-full min-h-fit gap-0 lg:gap-5 border-transparent hover:border dark:lg:hover:border-t-blue-900 dark:lg:hover:bg-slate-800/50 lg:hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:hover:drop-shadow-lg lg:hover:bg-slate-100/50 lg:hover:border-t-blue-200"
+          >
+            <CardHeader className="h-full w-full p-0">
+              <CardTitle className="text-base text-slate-400 whitespace-nowrap">
+                {job.timeline}
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="flex flex-col p-0">
+              <p className="text-foreground font-bold">
+                {job.currentPosition} • {job.place}
+              </p>
+              {job.previousPositions.map((position, index) => (
+                <p key={index} className="text-slate-400 text-sm font-bold">
+                  {position}
+                </p>
+              ))}
+              <CardDescription className="py-3 text-muted-foreground">
+                {job.description}
+              </CardDescription>
+              <CardFooter className="p-0 flex flex-wrap gap-2">
+                {job.skills.map((skill, index) => (
+                  <Badge key={index}>{skill}</Badge>
+                ))}
+              </CardFooter>
+            </CardContent>
+          </Card>
+        ))}
+      </>
+      <div className="lg:px-12 mt-12">
+        <a
+          href="mailto:rajamuzzamil9@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-flex items-center font-medium leading-tight text-foreground group"
+        >
+          <span className="border-b border-transparent pb-px transition hover:border-primary motion-reduce:transition-none">
+            Reach out for Full Resume
+          </span>
+          <MoveRight className="ml-1 inline-block h-5 w-5 shrink-0 -translate-y-px transition-transform group-hover:translate-x-2 group-focus-visible:translate-x-2 motion-reduce:transition-none" />
+        </a>
+      </div>
+    </section>
+  );
+}
